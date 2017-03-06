f19c20329525a3611e103bdef93cf8df6067bd05
SELISE\nahid.uzzaman <nahid.uzzaman@selise.ch>
2017-03-06 18:11:45 +0600

    mapservice

---
+++ controllers/about.controller.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+'use strict';
+
+	angular.module('starterApp')
+	    .controller('aboutController', constructor);
+	    	constructor.$inject = ['SampleService','$scope','$timeout','$document'];
+	    	function constructor(SampleService,$scope,$timeout,$document) {
+	    		var vm = this;
+
+	    		var geoPosition = {lat: 37.09, lng: -95.71};
+	    		
+                var gmap = SampleService.createMap('map',geoPosition);                
+                SampleService.createMarker(geoPosition,gmap);	 
+	        	
+	    	}
+
+})(window.angular);
---
+++ services/sample.service.js
@@ -0,0 +1,40 @@
+(function(angular) {
+'use strict';
+	
+	angular.module('starterApp')
+		.service('SampleService', constructor);
+		    function constructor() {
+		    	var teamList = ['South Africa',	'Australia', 'New Zealand', 'India', 'England',	'Sri Lanka',
+		    					'Bangladesh', 'Pakistan', 'West Indies', 'Afghanistan' , 'Zimbabwe', 'Ireland']
+				function getTeams(){
+			        return teamList
+			    };
+
+			    function createMap(htmlReferenceId,center) {
+		            var mapOptions = {
+		                zoom: 4,
+		                center: center,
+		                zoomControl: false,
+		                scaleControl: false,
+		                mapTypeControl: false,
+		                streetViewControl: false,
+		                styles: null
+		            }
+
+		            var map = new google.maps.Map(document.getElementById(htmlReferenceId), mapOptions);
+		            return map;
+		        }
+
+		        function createMarker(geoCoordinate,type){
+		        	var marker = new google.maps.Marker({
+			          position: geoCoordinate,
+			          map: type
+			        });
+		        }
+
+			    this.getTeams = getTeams;
+			    this.createMap = createMap;
+			    this.createMarker = createMarker; 
+		    }//End of constructor
+
+})(window.angular);
